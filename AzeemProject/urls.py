c84f0582d8d875c51d8a9b74953351af3f4781b8
haziqbutt <m.haziq.bese@gmail.com>
2025-04-17 22:02:53 +0500

    Add root Url

--- AzeemProject/urls.py
+++ AzeemProject/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello from the root URL!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('myapi.urls')),
+    path('', home), 
 ]
 
